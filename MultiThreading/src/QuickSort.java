301b365083f59de205deb0111d1d582e4f758cad
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-19 06:14:30 +0300

    extend the Thread class

---
+++ MultiThreading/src/QuickSort.java
@@ -0,0 +1,15 @@
+public class QuickSort extends Thread{
+
+    @Override
+    public void run() {
+        //here we will call our quick sort algorithm
+    }
+
+    public int[] quickSort(int[] arr, int start, int end)
+    {
+        /*This method will perform the sorting by recursion*/
+
+
+        return  null;
+    }
+}
