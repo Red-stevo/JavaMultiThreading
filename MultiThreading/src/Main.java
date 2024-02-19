d2cf889278563d05a0714914f7ea99548487f5a6
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-02-19 07:18:14 +0300

    configuring the multiple threads

--- MultiThreading/src/Main.java
+++ MultiThreading/src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args)
     {
+        int[] arr = new int[]{12,32,41,54,12,43,1,45,1,78,12,45,778,23,12,12,67,332,5,12,5,76,23,566,34,54,23,5,34,65,34};
 
+        QuickSort q = new QuickSort();
+
+        int point = q.partition(0,(arr.length-1));
     }
 }
--- MultiThreading/src/QuickSort.java
+++ MultiThreading/src/QuickSort.java
@@ -1,15 +1,45 @@
-public class QuickSort extends Thread{
+public class QuickSort {
 
-    @Override
-    public void run() {
-        //here we will call our quick sort algorithm
-    }
+    int[] arr;
 
-    public int[] quickSort(int[] arr, int start, int end)
+    public int[] quickSort(int start, int end)
     {
+        int point;
         /*This method will perform the sorting by recursion*/
+
+        //call the method to perform each partition(placement).
+//        point = partition(start, end);
+
 
 
         return  null;
+    }
+
+
+
+
+    public int partition(int start, int end) {
+
+        int pivot = start;
+
+        while (start < end)
+        {
+            while (arr[start] <= arr[pivot] && start < end) ++start;
+
+            while (arr[end] > arr[pivot] && start <= end) --end;
+
+            /*Performing the partition swap.*/
+            if(start < end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+            }
+        }
+        /*performing the position swap*/
+        int temp = arr[end];
+        arr[end] = arr[pivot];
+        arr[pivot] = temp;
+
+        return end;
     }
 }
